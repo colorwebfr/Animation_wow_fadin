bbd199d855d3792ed069dd021e7ba85827805410
Kevin Capoccetti <75384575+colorwebfr@users.noreply.github.com>
2024-02-15 17:31:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,31 @@
 /*---------------------------------------------------------------
 Anime any element inn viewport
 -----------------------------------------------------------------*/
-function animeInViewport(offset = 0) {
+export const animeInViewport = (offset = 0) => {
     $(document).ready(function () {
         const width_Screen = parseInt($(window).width());
         const heightScreen = parseInt($(window).height() - offset);
         //Only for desktop
         if (width_Screen > 1024) {
+            function wow_fade($this = this) {
+                switch (true) {
+                    case $this.hasClass('fadeInUp'):
+                        $this.css({ 'opacity': '1', 'transform': 'translateY(0)' });
+                        break;
+                    case $this.hasClass('fadeInDown'):
+                        $this.css({ 'opacity': '1', 'transform': 'translateY(0)' });
+                        break;
+                    case $this.hasClass('fadeInLeft'):
+                        $this.css({ 'opacity': '1', 'transform': 'translateX(0)' });
+                        break;
+                    case $this.hasClass('fadeInRight'):
+                        $this.css({ 'opacity': '1', 'transform': 'translateX(0)' });
+                        break;
+                    case $this.hasClass('fadeIn'):
+                        $this.css({ 'opacity': '1' });
+                        break;
+                }
+            }
             //event scroll
             if ($('body').find('wow')) {
                 //El appear on page loading
@@ -17,23 +36,7 @@ function animeInViewport(offset = 0) {
                     let wow = parseInt($(this).offset().top);
                     //Appear elements
                     if (win >= wow - heightScreen) {
-                        switch (true) {
-                            case $(this).hasClass('fadeInUp'):
-                                $(this).css({ 'opacity': '1', 'transform': 'translateY(0)' });
-                                break;
-                            case $(this).hasClass('fadeInDown'):
-                                $(this).css({ 'opacity': '1', 'transform': 'translateY(0)' });
-                                break;
-                            case $(this).hasClass('fadeInLeft'):
-                                $(this).css({ 'opacity': '1', 'transform': 'translateX(0)' });
-                                break;
-                            case $(this).hasClass('fadeInRight'):
-                                $(this).css({ 'opacity': '1', 'transform': 'translateX(0)' });
-                                break;
-                            case $(this).hasClass('fadeIn'):
-                                $(this).css({ 'opacity': '1' });
-                                break;
-                        }
+                        wow_fade($(this));
                     }
                 });
                 // END el appear on page loading
@@ -44,23 +47,7 @@ function animeInViewport(offset = 0) {
                         let wow = parseInt($(this).offset().top);
                         //Appear elements
                         if (scrollY > wow - heightScreen) {
-                            switch (true) {
-                                case $(this).hasClass('fadeInUp'):
-                                    $(this).css({ 'opacity': '1', 'transform': 'translateY(0)' });
-                                    break;
-                                case $(this).hasClass('fadeInDown'):
-                                    $(this).css({ 'opacity': '1', 'transform': 'translateY(0)' });
-                                    break;
-                                case $(this).hasClass('fadeInLeft'):
-                                    $(this).css({ 'opacity': '1', 'transform': 'translateX(0)' });
-                                    break;
-                                case $(this).hasClass('fadeInRight'):
-                                    $(this).css({ 'opacity': '1', 'transform': 'translateX(0)' });
-                                    break;
-                                case $(this).hasClass('fadeIn'):
-                                    $(this).css({ 'opacity': '1' });
-                                    break;
-                            }
+                            wow_fade($(this));
                         }
                     });
                 });
